9ec3f9a5afa22a36773306db98cb7525af9582f3
murad <muradkv@gmail.com>
2026-08-11 22:22:48 +0300

    Initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by murad on 11.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
