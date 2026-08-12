e8f7326b96838ca0be08e1033f8f6ea38502f637
murad <muradkv@gmail.com>
2026-08-12 23:47:00 +0300

    Explore basic Core Image integration using optional Image state
    - Add sample image asset to project bundle.
    - Render optional Image conditionally inside ContentView body.
    - Load sample image dynamically using .onAppear modifier to simulate async image processing pipeline.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        image = Image(.example)
     }
 }
 
